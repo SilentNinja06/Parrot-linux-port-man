3eb416dccd66779452acd8ab2cf143ce23f4fbe2
Sebastian B <98119533+SilentNinja06@users.noreply.github.com>
2022-10-17 09:12:51 -0700

    Created check.py
    Testing a function of github

---
+++ check.py
@@ -0,0 +1,13 @@
+def get_avg(*values):
+  if len(values) == 0:
+    return 0
+  
+  sum_v = 0
+  for value in values:
+    sum_v += value
+    return sum_v / len(values)
+
+avg = get_avg(1,2,3,4)
+#avg is 2.5
+
+print avg
